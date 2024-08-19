ad4a76607b5a1e0c466bbdc9264e5405b10f0b7c
Ewelyn Sâmaris <ewelynsamaris@hotmail.com>
2024-08-19 16:16:19 -0300

    chore: modify AppError entity for better legebility

--- src/domain/entities/app-error.entity.ts
+++ src/domain/entities/app-error.entity.ts
@@ -1,16 +1,17 @@
 import { v4 as uuidv4 } from 'uuid';
 import { ErrorContext } from '../enums/error-context.enum';
 
-export class AppError {
+export class AppError extends Error {
   static errors: AppError[] = [];
   public id: string;
-  public error: string;
+  public message: string;
   public errorContext: ErrorContext;
   public createdAt: Date;
 
   constructor(errorMessage: string, errorContext: ErrorContext) {
+    super();
     this.id = uuidv4();
-    this.error = errorMessage;
+    this.message = errorMessage;
     this.errorContext = errorContext;
     this.createdAt = new Date();
 
