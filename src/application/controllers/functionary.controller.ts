5a073deab09a0876c3ae68a7ac520c32d7bc0629
Ewelyn Sâmaris <ewelynsamaris@hotmail.com>
2024-08-19 23:21:07 -0300

    feat: adds interfaces as inboud ports to controllers

--- src/application/controllers/functionary.controller.ts
+++ src/application/controllers/functionary.controller.ts
@@ -1,22 +1,22 @@
-import { Controller, UsePipes } from '@nestjs/common';
+import { Controller, Inject, Param, UsePipes } from '@nestjs/common';
 import { Post, Body, Get } from '@nestjs/common';
-import { HttpStatus, InternalServerErrorException } from '@nestjs/common';
+import { HttpStatus } from '@nestjs/common';
 import { DataFormatterAdapter } from '../../infrastructure/adapters/formatDateTime.adapter';
 import { AppResponse } from '../../domain/models/app-response.model';
 import { CreateFunctionaryDTO } from '../dtos/create-functionary.dto';
-import { Functionary } from '../../domain/entities/functionary.entity';
 import { CreateFunctionaryValidationPipe } from '../validators/create-functionary-validation.pipe';
-import { FunctionaryService } from '../../domain/services/functionary.service';
+import { IFunctionaryService } from '../../domain/interfaces/functionary/functionary-service.interface';
+import { Functionary } from '../../domain/entities/functionary/functionary.entity';
+import { NationalIdentifierValidationPipe } from '../validators/national-identifier-validation.pipe';
 
 @Controller('v1/functionaries')
 export class FunctionaryController {
-  constructor(private readonly functionaryService: FunctionaryService) {}
+  constructor(@Inject('IFunctionaryService') private readonly iFunctionaryService: IFunctionaryService) {}
 
   @Get()
   getAllFunctionaries(): AppResponse {
     try {
-      const functionaries = this.functionaryService.getFunctionaries();
-
+      const functionaries = this.iFunctionaryService.getFunctionaries();
       return {
         statusCode: HttpStatus.OK,
         message: 'All functionaries retrieved successfully',
@@ -24,7 +24,25 @@ export class FunctionaryController {
         data: functionaries,
       };
     } catch (error) {
-      throw new InternalServerErrorException("Can't retrieve functionaries. Internal server error");
+      return {
+        statusCode: HttpStatus.NOT_FOUND,
+        message: `No functionaries found`,
+        date: DataFormatterAdapter.formatDateTimeString(),
+      };
+    }
+  }
+
+  @Get(':cpf')
+  @UsePipes(NationalIdentifierValidationPipe)
+  getFunctionaryById(@Param() cpf: string): AppResponse {
+    try {
+      this.iFunctionaryService.getFunctionaryByCpf(cpf);
+    } catch (error) {
+      return {
+        statusCode: HttpStatus.NOT_FOUND,
+        message: `No functionary found with the given cpf #${cpf}`,
+        date: DataFormatterAdapter.formatDateTimeString(),
+      };
     }
   }
 
@@ -32,7 +50,7 @@ export class FunctionaryController {
   @UsePipes(CreateFunctionaryValidationPipe)
   create(@Body() createFunctionaryDTO: CreateFunctionaryDTO): AppResponse {
     try {
-      const functionary: Functionary = this.functionaryService.createFunctionary(createFunctionaryDTO);
+      const functionary: Functionary = this.iFunctionaryService.createFunctionary(createFunctionaryDTO);
       return {
         statusCode: HttpStatus.CREATED,
         message: 'Functionary created successfully',
@@ -40,7 +58,11 @@ export class FunctionaryController {
         data: functionary,
       };
     } catch (error) {
-      throw new InternalServerErrorException("Can't create functionary. Internal server error");
+      return {
+        statusCode: error.getStatus(),
+        message: error.message,
+        date: DataFormatterAdapter.formatDateTimeString(),
+      };
     }
   }
 }
