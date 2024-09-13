da33a4351b3e30def9831f6e8e6f76659d82bbb7
Ewelyn Sâmaris <ewelynsamaris@hotmail.com>
2024-09-13 20:13:37 -0300

    feature: adds database configurations to app module

--- app.module.ts
+++ app.module.ts
@@ -1,12 +1,32 @@
 import { Module } from '@nestjs/common';
 import { ApplicationModule } from './src/application/applicationModule';
-
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get<string>('POSTGRES_HOST'),
+        port: parseInt(configService.get<string>('POSTGRES_PORT')),
+        username: configService.get<string>('POSTGRES_USER'),
+        password: configService.get<string>('POSTGRES_PASSWORD'),
+        database: configService.get<string>('POSTGRES_DB'),
+        entities: [
+          'src/domain/entities/**/*.entity.{ts,js}',
+        ],
+        synchronize: true,
+        logging: ['query', 'error', 'schema'],
+      })
+    }),
    ApplicationModule,
   ],
-  controllers: [],
-  providers: [],
 })
 export class AppModule {}
